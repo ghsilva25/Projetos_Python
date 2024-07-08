1fefd3e8f7704ed7327f25a97ab8d758c982fd19
Gabriel Silva <114780718+ghsilva25@users.noreply.github.com>
2024-07-08 09:31:21 -0300

    Criando arquivo.py
    O objetivo deste código é criar um arquivo txt.

---
+++ Criando arquivo.py
@@ -0,0 +1,10 @@
+# Criando arquivo com extensão txt. 
+
+# ETAPA 1: Criando a extensão.
+arq = open('teste.txt', 'w')
+
+# ETAPA 2: Atribuindo o testo no arquivo txt.
+arq.write('Olá, meu nome é Gabriel! Estou aprendendo a linguagem Python...')
+
+#ETAPA 3: Finalizando.
+arq.close()
